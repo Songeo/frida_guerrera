d19eb985f0fe01592d3357329d2a09a0deaae247
Songeo <sonia.gmcl@gmail.com>
2019-03-31 20:12:45 -0600

    stop words tbl

---
+++ munge/stop_words.R
@@ -0,0 +1,22 @@
+
+
+stpwds_nonexists <- (list.files("cache/", pattern = "stop_words_tbl") %>% 
+                       length()) == 0
+
+if(stpwds_nonexists){
+  # https://github.com/stopwords-iso/stopwords-es
+  sw <- read_table("https://raw.githubusercontent.com/stopwords-iso/stopwords-es/master/stopwords-es.txt", 
+             col_names = c("word"))
+  
+  stop_words_tbl <- 
+    read_delim("docs/stopwords_countswords.txt", 
+             delim = "\t", 
+             col_names = c("row_num", "word")) %>% 
+    dplyr::select(-row_num) %>% 
+    bind_rows(sw) %>% 
+    bind_rows(tibble(word = c("2016", '2017'))) %>% 
+    unique()
+  
+  rm("sw")
+  save(stop_words_tbl, file = "cache/stop_words_tbl.RData")
+}
